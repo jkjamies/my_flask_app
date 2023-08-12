414c037d565d30766286015bec9e2f76e60b34ef
Jason Jamieson <69278911+jkjamies@users.noreply.github.com>
2023-08-12 11:39:42 -0600

    feat: add weather fetches by city name (#4)
    * feat: add weather fetches by city name
    
    * fix: update requirements.txt
    
    * fix: build workflow update for .env

--- applications/frontend/app.py
+++ applications/frontend/app.py
@@ -1,25 +1,61 @@
-#!/usr/bin/env python3
+"""Frontend application for weather search by city"""
 
-"""A simple Flask frontend."""
+import os
+from datetime import datetime
+from flask import Flask, render_template, request
+import requests
+from dotenv import load_dotenv
 
-from flask import Flask, request
+load_dotenv()
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def main():
-    """Return the main page."""
-    return '''
-     <form action="/echo_user_input" method="POST">
-         <input name="user_input">
-         <input type="submit" value="Submit!">
-     </form>
-     '''
+OPENWEATHER_API_KEY = os.getenv('OPENWEATHER_API_KEY')
 
 
-@app.route("/echo_user_input", methods=["POST"])
-def echo_input():
-    """Echo the user input."""
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+@app.route('/')
+def index():
+    """Setting frontend index.html"""
+    return render_template('index.html')
+
+
+@app.route('/get_3day_forecast', methods=['POST'])
+def get_3day_forecast():
+    """Get the 3-day forecast"""
+    city = request.form['city']
+
+    if not city:
+        return render_template('index.html', error='City not provided')
+
+    url = (f'http://api.openweathermap.org/data/2.5/forecast?q={city}'
+           f'&appid={OPENWEATHER_API_KEY}'
+           f'&units=metric')
+    response = requests.get(url, timeout=3000)
+    data = response.json()
+
+    if data.get('cod') != '200':
+        return render_template('index.html', error='City not found')
+
+    forecast = []
+    for entry in data['list']:
+        forecast_entry = {
+            'date': format_date(entry['dt_txt']),
+            'temperature': entry['main']['temp'],
+            'description': entry['weather'][0]['description'],
+            'icon': entry['weather'][0]['icon'],
+        }
+        forecast.append(forecast_entry)
+
+    return render_template('index.html', city=city, forecast=forecast)
+
+
+def format_date(dt_txt):
+    """Format the date for friendly reading"""
+    dt_object = datetime.strptime(dt_txt, '%Y-%m-%d %H:%M:%S')
+    formatted_date = (dt_object.strftime('%A, %B %d, %Y %I:%M %p')
+                      .replace(" 0", " "))
+    return formatted_date
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- tests/frontend/test_app_unittest.py
+++ tests/frontend/test_app_unittest.py
@@ -1,24 +1,35 @@
-"""Test the frontend.py file using unit test."""
+"""Unit tests for the frontend."""
 
 import unittest
 from applications.frontend.app import app
 
 
 class TestApp(unittest.TestCase):
-    """Test the frontend.py file using unit test."""
+    """Unit tests for the frontend."""
 
     def setUp(self):
         """Set up the test client."""
         self.app = app.test_client()
+        self.app.testing = True
 
-    def test_main_route(self):
-        """Test the main route."""
+    def test_index_route(self):
+        """Test the index route."""
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(
-            b'<form action="/echo_user_input" method="POST">',
-            response.data
-        )
+        self.assertIn(b'3-Day Weather Forecast App', response.data)
+
+    def test_get_3day_forecast(self):
+        """Test the get_3day_forecast route."""
+        response = self.app.post('/get_3day_forecast',
+                                 data={'city': 'New York'})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'3-Day Weather Forecast for New York', response.data)
+
+    def test_invalid_city(self):
+        """Test the get_3day_forecast route with invalid city."""
+        response = self.app.post('/get_3day_forecast', data={'city': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'City not provided', response.data)
 
 
 if __name__ == '__main__':
