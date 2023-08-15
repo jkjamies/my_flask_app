1022d54364679f41143c7fc5db4413917c3336f7
Jason Jamieson <69278911+jkjamies@users.noreply.github.com>
2023-08-14 19:38:36 -0600

    feat: metric and health endpoints (#9)
    * feat: added health and metrics enpoints
    
    * fix: update requirements.txt
    
    * chore: add tests
    
    * fix: code style
    
    * fix: operation and testing
    
    * fix: use gauge not counter
    
    * fix: build

--- applications/frontend/app.py
+++ applications/frontend/app.py
@@ -1,10 +1,18 @@
 """The frontend application."""
 
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
+from prometheus_client import Counter, CollectorRegistry, generate_latest
+import time
 from ..data_analyzer.data_analyzer import DataAnalyzer
 
 app = Flask(__name__)
-data_analyzer = DataAnalyzer()  # Create an instance of the DataAnalyzer
+data_analyzer = DataAnalyzer()
+
+registry = CollectorRegistry()
+REQUEST_COUNT = Counter("http_requests_per_second",
+                        "Requests per second since app start",
+                        ["method"], registry=registry)
+start_time = time.time()
 
 
 @app.route('/')
@@ -12,6 +20,40 @@ def index():
     """Render the index.html template with weather data."""
     weather_data = data_analyzer.get_weather_data()
     return render_template('index.html', weather_data=weather_data)
+
+
+@app.route('/health', methods=['GET'])
+def health():
+    """Return a simple health check"""
+    return "", 200
+
+
+@app.route('/metrics', methods=['GET'])
+def metrics():
+    """Return the prometheus metrics"""
+    current_time = time.time()
+    time_difference = current_time - start_time
+
+    total_requests = REQUEST_COUNT.labels(method="GET")._value.get()
+
+    if time_difference > 0:
+        requests_per_second = total_requests / time_difference
+    else:
+        requests_per_second = 0
+
+    response_data = {
+        "requests_per_second": requests_per_second
+    }
+
+    metrics_data = generate_latest(registry=registry)
+    response_data["prometheus_data"] = metrics_data.decode('utf-8')
+
+    return jsonify(response_data), 200
+
+
+@app.before_request
+def before_request():
+    REQUEST_COUNT.labels(method="GET").inc()
 
 
 if __name__ == '__main__':
--- tests/frontend/test_app_unittest.py
+++ tests/frontend/test_app_unittest.py
@@ -1,19 +1,32 @@
 """Unit tests for the frontend app."""
 
+import json
 import unittest
-from applications.data_analyzer.data_analyzer import DataAnalyzer
+from applications.frontend.app import app
 
 
-@DeprecationWarning
 class TestApp(unittest.TestCase):
-    """Unit tests for the frontend app."""
+    """Unit tests for the frontend app. [Deprecated]"""
 
-    def test_format_date(self):
-        """Test that we can format a date"""
-        dt_txt = '2023-08-11 12:00:00'
-        formatted_date = DataAnalyzer().format_date(dt_txt)
-        expected_date = 'Friday, August 11, 2023 12:00 PM'
-        self.assertEqual(formatted_date, expected_date)
+    def setUp(self):
+        """Set up the test client"""
+        self.app = app.test_client()
+
+    def test_health_check(self):
+        """Test that the health check endpoint works as expected"""
+        response = self.app.get('/health')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), "")
+
+    def test_metrics_endpoint(self):
+        """Test the /metrics endpoint"""
+        response = self.app.get('/metrics')
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data.decode('utf-8'))
+        self.assertIn('requests_per_second', data)
+        self.assertIsInstance(data['requests_per_second'], (int, float))
+        self.assertIn('prometheus_data', data)
+        self.assertIsInstance(data['prometheus_data'], str)
 
 
 if __name__ == '__main__':
